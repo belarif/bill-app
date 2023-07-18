08ce4c8830da35a71bca0a5fa7905807af71f79e
Belarif <b.ocine@live.fr>
2023-07-18 11:57:26 +0200

    main: test displaying title when i am on NewBill page

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -4,15 +4,13 @@
 
 import { screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
-
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
+    test("should display, 'Envoyer une note de frais'", () => {
       const html = NewBillUI()
       document.body.innerHTML = html
-      //to-do write assertion
+      expect(screen.getByTestId("title").textContent).toEqual("Envoyer une note de frais")
     })
   })
 })
