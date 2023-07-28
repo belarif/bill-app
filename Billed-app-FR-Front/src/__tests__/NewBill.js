601de5e3c4ac20983429a0b59dc1884d2a675988
Belarif <b.ocine@live.fr>
2023-07-28 16:00:42 +0200

    main : test that handleChangeFile() is callable

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -4,14 +4,30 @@
 
 import NewBill from "../containers/NewBill";
 import NewBillUI from "../views/NewBillUI";
+import { localStorageMock } from "../__mocks__/localStorage";
+import { fireEvent, screen } from "@testing-library/dom";
 
 describe('Given i am connected as an employee', () => {
   describe('When i am on new bill page', () => {
     test('That NewBill class is instantiable', () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
+      document.body.innerHTML = NewBillUI();
       const newBillObjet = new NewBill({ document, onNavigate : {}, store : {}, localStorage : {}});
       expect(newBillObjet).toBeInstanceOf(NewBill);
+    })
+
+    test('That handleChangeFile() function is callable when change event is triggered', () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+
+      document.body.innerHTML = NewBillUI()
+      const newBillObjet = new NewBill({ document, onNavigate : {}, store : {}, localStorage : {}});
+      const handleChange = jest.fn((e) => newBillObjet.handleChangeFile(e))
+      const inputFile = screen.getByTestId('file')
+      inputFile.addEventListener('change', handleChange)
+      fireEvent.change(inputFile)
+      expect(handleChange).toHaveBeenCalled()
     })
   })
 })
