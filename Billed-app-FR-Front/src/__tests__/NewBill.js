c2b7e04bd48f8e089ef226ebaacb1804ca7655b8
Belarif <b.ocine@live.fr>
2023-07-28 16:59:47 +0200

    main: test create newBill function

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -6,6 +6,7 @@ import NewBill from "../containers/NewBill";
 import NewBillUI from "../views/NewBillUI";
 import { localStorageMock } from "../__mocks__/localStorage";
 import { fireEvent, screen } from "@testing-library/dom";
+import { ROUTES, ROUTES_PATH } from "../constants/routes";
 
 describe('Given i am connected as an employee', () => {
   describe('When i am on new bill page', () => {
@@ -28,6 +29,39 @@ describe('Given i am connected as an employee', () => {
       inputFile.addEventListener('change', handleChange)
       fireEvent.change(inputFile)
       expect(handleChange).toHaveBeenCalled()
+    })
+  })
+
+  describe('When i am on new bill page, i do fill fields in correct format and i click submit button', () => {
+    test('That new bill is created', () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+
+      document.body.innerHTML = NewBillUI()
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+
+      const storeMock = {
+        bills: () => {
+          return {
+            update: (bill) => {
+              return {
+                then: (fn) => fn(bill)
+              };
+            },
+          };
+        },
+      };
+      
+      const newBillObjet = new NewBill({ document, onNavigate, store : storeMock, localStorage : window.localStorage });
+      const form = screen.getByTestId("form-new-bill");
+      const handleSubmit = jest.fn((e) => newBillObjet.handleSubmit(e));
+      form.addEventListener("submit", handleSubmit);
+      fireEvent.submit(form);
+      expect(form).toBeTruthy();
     })
   })
 })
