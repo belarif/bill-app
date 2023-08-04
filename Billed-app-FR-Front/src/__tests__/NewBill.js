bdd67f71fef7fada856be726c96329705348faec
Belarif <b.ocine@live.fr>
2023-08-04 19:57:49 +0200

    main: delete unnecessary test

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -6,17 +6,11 @@ import NewBill from "../containers/NewBill";
 import NewBillUI from "../views/NewBillUI";
 import { localStorageMock } from "../__mocks__/localStorage";
 import { fireEvent, screen } from "@testing-library/dom";
-import { ROUTES, ROUTES_PATH } from "../constants/routes";
-import router from "../app/Router"
+import { ROUTES } from "../constants/routes";
+import router from "../app/Router";
 
 describe('Given i am connected as an employee', () => {
   describe('When i am on new bill page', () => {
-    test('That NewBill class is instantiable', () => {
-      document.body.innerHTML = NewBillUI();
-      const newBillObjet = new NewBill({ document, onNavigate : {}, store : {}, localStorage : {}});
-      expect(newBillObjet).toBeInstanceOf(NewBill);
-    })
-
     test('That handleChangeFile() function is callable when change event is triggered', () => {
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
       window.localStorage.setItem('user', JSON.stringify({
